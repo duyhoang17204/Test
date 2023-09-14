72004c437c833a8b8a5ee8f31b23df0924ded28f
Mac <mac@Macs-MacBook-Pro-2.local>
2023-09-14 11:45:34 +0700

    bt

---
+++ 3.js
@@ -0,0 +1,22 @@
+// document.write("xin chao");
+
+// function abc() {
+//   alert("thong bao");
+// }
+// var n = 1,
+//   m = "1";
+// if (n == m) document.write("bang nhau");
+// else document.write("khong bang");
+
+// var nut = document.querySelector(".nut");
+// var khoi = document.querySelector(".khoi");
+// nut.onclick = function () {
+//   khoi.classList.toggle("doimau");
+// };
+
+var dn = document.querySelector(".dn");
+var login = document.querySelector(".login");
+
+dn.onclick = function () {
+  login.classList.toggle("active");
+};
